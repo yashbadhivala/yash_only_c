db19f63c9bddf8ae535c61807c0c29f9575045f6
vivek7669 <chauhanvivek0918@gmail.com>
2025-06-11 12:26:18 +0530

    created

---
+++ yash.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+
+    int a,b;
+
+    
+    
+    printf("enter no. 1 :");
+    scanf("%d",&a);
+
+    
+    printf("enter no. 2 :");
+    scanf("%d",&b);
+
+    printf("%d",a+b);
+
+    return 0;
+}
